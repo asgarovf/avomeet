7f823926b60c7df8c9bd85c0ce88ded526be672b
asgarovf <fasgarov2002@gmail.com>
2023-12-18 05:15:45 +0300

    feat: add navbar to rejoin page

--- src/components/screens/LeaveScreen.js
+++ src/components/screens/LeaveScreen.js
@@ -1,19 +1,23 @@
 import { Button } from "../../ui/Button";
+import { Navbar } from "../Navbar";
 
 export function LeaveScreen({ setIsMeetingLeft }) {
   return (
-    <div className="bg-gray-800 h-screen flex flex-col flex-1 items-center justify-center">
-      <h1 className="text-white text-4xl">You left the meeting</h1>
-      <div className="mt-12">
-        <Button
-          className="w-full"
-          onClick={() => {
-            setIsMeetingLeft(false);
-          }}
-        >
-          Rejoin the Meeting
-        </Button>
+    <>
+      <Navbar />
+      <div className="bg-gray-800 h-[90vh] flex flex-col flex-1 items-center justify-center">
+        <h1 className="text-white text-4xl">You left the meeting</h1>
+        <div className="mt-12">
+          <Button
+            className="w-full"
+            onClick={() => {
+              setIsMeetingLeft(false);
+            }}
+          >
+            Rejoin the Meeting
+          </Button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
